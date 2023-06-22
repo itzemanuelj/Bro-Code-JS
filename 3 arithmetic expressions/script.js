ee063782d58130ba909e3b1404eaee6ab92e5da3
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-22 12:56:04 -0400

    commit to bro code arithmetic expressions in js

---
+++ 3 arithmetic expressions/script.js
@@ -0,0 +1,17 @@
+/// arithmetic expressions
+let student = 20;
+
+student += 1;
+console.log(student);
+
+student -= 1;
+console.log(student);
+
+let extraStudent = student % 3;
+console.log(extraStudent);
+
+student *= 2;
+console.log(student);
+
+let result = 1 + 2 * (3 + 4);
+console.log(restult);
