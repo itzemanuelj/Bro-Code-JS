a90af76a024f0dcdc7accd61e0e8d269ddebf37f
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-22 00:27:53 -0400

    commit to bro code variables in js

--- 2 variables/script.js
+++ 2 variables/script.js
@@ -1,1 +1,19 @@
-console.log("hello world!");
+/// variables
+
+let age;
+age = 28;
+console.log(age);
+
+let fname = "emanuel";
+let student = true;
+
+console.log(`hello my name is ${fname} i am ${age} years old`);
+
+age = age + 1;
+age += 1;
+
+console.log("hello " + " " + fname);
+console.log("you are " + " " + age + " years old");
+console.log("are you a student:", student);
+
+document.getElementById("demo").innerHTML = "Hello World";
