f0b3fda7a9a8290f7f57e60ca8b703e87832e158
Emanuel Joness <itzemanuelj@gmail.com>
2023-06-25 00:52:22 -0400

    commit to brocode how to use const in js

---
+++ 6 const/script.js
@@ -0,0 +1,11 @@
+/// const
+
+const PI = 3.14;
+let radius;
+let circle;
+
+radius = prompt("enter radius of circle");
+radius = Number(radius);
+
+circle = 2 * PI * radius;
+console.log(`this circ is,`, circle);
